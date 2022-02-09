2e89978c59c00d1ece437ae22838456f304e8f63
ummerukaya <ummerukaya13@gmail.com>
2022-02-09 23:22:38 +0600

    mapper added

--- src/Plugins/Nop.Plugin.Widgets.MySlider/Areas/Admin/Infrastructure/MapperConfiguration.cs
+++ src/Plugins/Nop.Plugin.Widgets.MySlider/Areas/Admin/Infrastructure/MapperConfiguration.cs
@@ -3,10 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AutoMapper;
+using Nop.Core.Infrastructure.Mapper;
+using Nop.Plugin.MySlider.Domains;
+using Nop.Plugin.Widgets.MySlider.Areas.Admin.Models;
+using Nop.Plugin.Widgets.MySlider.Models;
 
 namespace Nop.Plugin.Widgets.MySlider.Areas.Admin.Infrastructure
 {
-    public class MapperConfiguration
+    public class MapperConfiguration : Profile, IOrderedMapperProfile
     {
+        public int Order => 1;
+
+        public MapperConfiguration()
+        {
+            CreateMap<MySliderSettings, ConfigurationModel>()
+                   .ForMember(model => model.EnableSlider_OverrideForStore, options => options.Ignore())
+                   .ForMember(model => model.CustomProperties, options => options.Ignore())
+                   .ForMember(model => model.ActiveStoreScopeConfiguration, options => options.Ignore());
+            CreateMap<ConfigurationModel, MySliderSettings>();
+
+            CreateMap<MySliders, MySliderModel>()
+                    .ForMember(model => model.AvailableStores, options => options.Ignore())
+                    .ForMember(model => model.AvailableWidgetZones, options => options.Ignore())
+                    .ForMember(model => model.WidgetZoneStr, options => options.Ignore())
+                    .ForMember(model => model.CreatedOn, options => options.Ignore())
+                    .ForMember(model => model.UpdatedOn, options => options.Ignore())
+                    .ForMember(model => model.SliderItemSearchModel, options => options.Ignore())
+                    .ForMember(model => model.CustomProperties, options => options.Ignore())
+                    .ForMember(model => model.SelectedStoreIds, options => options.Ignore());
+            CreateMap<MySliderModel, MySliders>()
+                    .ForMember(entity => entity.CreatedOnUtc, options => options.Ignore())
+                    .ForMember(entity => entity.UpdatedOnUtc, options => options.Ignore());
+
+            CreateMap<MySliderItem, MySliderItemModel>()
+                    .ForMember(model => model.CustomProperties, options => options.Ignore())
+                    .ForMember(model => model.FullPictureUrl, options => options.Ignore())
+                    .ForMember(model => model.PictureUrl, options => options.Ignore())
+                    .ForMember(model => model.MobileFullPictureUrl, options => options.Ignore())
+                    .ForMember(model => model.MobilePictureUrl, options => options.Ignore());
+            CreateMap<MySliderItemModel, MySliderItem>();
+        }
     }
 }
--- src/Plugins/Nop.Plugin.Widgets.MySlider/Areas/Admin/Models/ConfigurationModel.cs
+++ src/Plugins/Nop.Plugin.Widgets.MySlider/Areas/Admin/Models/ConfigurationModel.cs
@@ -3,10 +3,17 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Nop.Web.Framework.Models;
+using Nop.Web.Framework.Mvc.ModelBinding;
 
 namespace Nop.Plugin.Widgets.MySlider.Areas.Admin.Models
 {
-    class ConfigurationModel
+    public record ConfigurationModel : BaseNopModel, ISettingsModel
     {
+        public int ActiveStoreScopeConfiguration { get ; set ; }
+
+        [NopResourceDisplayName("Nop.MySlider.Configuration.Fields.EnableSlider")]
+        public bool EnableSlider { get; set; }
+        public bool EnableSlider_OverrideForStore { get; set; }
     }
 }
